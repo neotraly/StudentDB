c9820184b173cc6424fad9de9e959f1deafe9a16
Dark Lil <denis.zylev2016@gmail.com>
2021-05-12 10:39:54 +0300

    -add database

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace StudendDB
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
